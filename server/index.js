a614f53e0fb937df3f36d62d8c40116e57bbb26a
Maverick D. Aece <41968336+Maverick-D-Aece@users.noreply.github.com>
2021-03-03 23:16:57 +0530

    Added demo code for Todo using Mongoose .save() to save a todo to MongoDB

--- server/index.js
+++ server/index.js
@@ -6,9 +6,22 @@ mongoose.connect("mongodb://localhost/test", {
 	useUnifiedTopology: true,
 });
 
+const Todo = mongoose.model("Todo", {
+	text: String,
+	complete: Boolean,
+});
+
 const typeDefs = `
   type Query {
     hello(name: String): String!
+  }  
+  type Todo {
+    id: ID!  
+    text: String!
+    complete: Boolean!
+  }
+  type Mutation {
+      createTodo(text: String!): Todo
   }
 `;
 
@@ -16,7 +29,19 @@ const resolvers = {
 	Query: {
 		hello: (_, { name }) => `Hello ${name || "World"}`,
 	},
+	Mutation: {
+		createTodo: async (_, { text }) => {
+			const todo = new Todo({ text, complete: false });
+			await todo.save();
+			return todo;
+		},
+	},
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log("Server is running on localhost:4000"));
+
+mongoose.connection.once("open", function () {
+	server.start(() =>
+		console.log("Server is running on localhost:4000 | Mongoose Connected")
+	);
+});
